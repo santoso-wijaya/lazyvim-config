af362f9f3f25edec2aa85e5bfedfb4b55ec02771
swijaya <swijaya@google.com>
2024-03-20 21:28:44 +0000

    Add `mini.move` plugin
    Move selections around with `<M-hjkl>`.

---
+++ lua/plugins/mini-move.lua
@@ -0,0 +1,5 @@
+-- Move any selection in any direction with <M-hjkl>.
+return {
+  { "echasnovski/mini.move", version = "*" },
+  -- Default config: https://github.com/echasnovski/mini.move?tab=readme-ov-file#default-config
+}
