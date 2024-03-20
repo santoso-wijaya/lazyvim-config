0369835c5dd8131921b37cc069c5e3eaa997ce7d
Santoso Wijaya <code@swijaya.me>
2024-03-19 21:52:30 -0700

    Add NeoVide settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,36 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.o.guifont = "MonaspiceNe Nerd Font,FiraCode Nerd Font,Monaspace Neon,Fira Code"
+
+if vim.g.neovide then
+  vim.g.neovide_theme = "auto" -- dark/light matches current system settings
+  vim.g.neovide_input_macos_alt_is_meta = true
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  vim.g.neovide_remember_window_size = true
+
+  -- Only enables IME in input mode and when searching, so that you can navigate
+  -- normally (e.g. when typing Japanese)
+  local function set_ime(args)
+    if args.event:match("Enter$") then
+      vim.g.neovide_input_ime = true
+    else
+      vim.g.neovide_input_ime = false
+    end
+  end
+
+  local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+
+  vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+    group = ime_input,
+    pattern = "*",
+    callback = set_ime,
+  })
+
+  vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+    group = ime_input,
+    pattern = "[/\\?]",
+    callback = set_ime,
+  }) -- Put anything you want to happen only in Neovide here
+end
