27dec807c52bfbc0ab6e56dccf475e37b78892ad
Santoso Wijaya <code@swijaya.me>
2024-03-20 17:17:18 -0700

    Add `nvim-colorizer.lua` plugin
    `:ColorizerToggle`

---
+++ lua/plugins/nvim-colorizer.lua
@@ -0,0 +1,5 @@
+-- Commands (for filetypes that aren't hooked in AutoCmd)
+-- * :ColorizerAttachToBuffer
+-- * :ColorizerDetachFromBuffer
+-- * :ColorizerToggle
+return { "norcalli/nvim-colorizer.lua" }
