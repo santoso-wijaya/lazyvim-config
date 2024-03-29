fc762d65f49f71ac9d5aea974cbb6bf75eac4c75
Santoso Wijaya <code@swijaya.me>
2024-03-28 23:06:38 -0700

    Tweak solarized/selenized opts

--- lua/plugins/solarized.lua
+++ lua/plugins/solarized.lua
@@ -8,6 +8,7 @@ return {
     priority = 1000,
     -- See: https://www.lazyvim.org/configuration/plugins#%EF%B8%8F-customizing-plugin-specs
     opts = {
+      -- See: https://github.com/maxmx03/solarized.nvim/blob/main/lua/solarized/palette.lua
       palette = "selenized",
       -- See: https://github.com/maxmx03/solarized.nvim?tab=readme-ov-file#default-config
       styles = {
@@ -22,12 +23,9 @@ return {
       highlights = function(colors)
         return {
           -- Highlight of indent character.
-          -- Use a more subdued background highlight hue (in the default dark mode base).
-          IblIndent = { fg = colors.base02, nocombine = true },
+          IblIndent = { fg = colors.base01, nocombine = true },
           -- Highlight of indent character when base of current context.
-          -- Use a more subdued comment hue (in the default dark mode base).
-          -- Note that the base is in the default dark mode; it will be flipped in light mode.
-          IblScope = { fg = colors.base01, nocombine = true },
+          IblScope = { fg = colors.base02, nocombine = true },
         }
       end,
     },
