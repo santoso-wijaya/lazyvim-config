b3b54830fc3764c16c40ec9b8e40089866605c07
swijaya <swijaya@google.com>
2024-05-20 12:49:56 +0000

    Clean up plugins config files

--- lua/plugins/mini-move.lua
+++
@@ -1,5 +0,0 @@
--- Move any selection in any direction with <M-hjkl>.
-return {
-  { "echasnovski/mini.move", version = "*" },
-  -- Default config: https://github.com/echasnovski/mini.move?tab=readme-ov-file#default-config
-}
