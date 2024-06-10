8347eef8a8cffd63893def1bccdeb64279f10715
Santoso Wijaya <code@swijaya.me>
2024-06-10 18:51:22 +0000

    Plugin updates

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,7 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       setup = {
+        -- Disable rust-analyzer setup since it conflicts with mason's
         rust_analyzer = function()
           return true
         end,
