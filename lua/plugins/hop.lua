f1a037efde967f414a39074069870c44c257bd7d
Santoso Wijaya <code@swijaya.me>
2024-07-26 10:25:19 -0700

    Update lazy.nvim 11.14.1 and LazyVim 12.38.2
    Plus plugin and extra updates, and updated configs.

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -32,8 +32,8 @@ return {
   {
     "folke/which-key.nvim",
     opts = {
-      defaults = {
-        ["<leader>h"] = { name = "+hop" },
+      specs = {
+        { "<leader>h", group = "hop" },
       },
     },
   },
